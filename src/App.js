5c7d25347cdc9f6fc80939f430e23f8a5ea5d6b8
Arash Cipriano Cardoso Kaffashi <arash_kaffashi@hotmail.com>
2022-11-09 13:58:42 -0400

    done: Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-// src/App.js
 import './App.css';
+import foods from './foods.json';
+import { Divider } from 'antd';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Divider>Food List</Divider>
+      {foods.map(({ name, image }) => {
+        return (
+          <div>
+            <h3>{name}</h3>
+            <img src={image} height={'60px'} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 export default App;
